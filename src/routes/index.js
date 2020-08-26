2b08e4c598b6a4f2e62b3605355d12b5aeda7dd9
NoerNova <noernova666@gmail.com>
2020-08-26 16:08:21 +0700

    Fix router

--- src/routes/index.js
+++ src/routes/index.js
@@ -10,17 +10,19 @@ import { Home, About, Blogs, Works, Contact, NotFoundPage } from "../pages";
 
 function Routes() {
   return (
-    <Switch>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/home">
-        <Redirect to="/" />
-      </Route>
-      <Route path="/about" component={About} />
-      <Route path="/blogs" component={Blogs} />
-      <Route path="/works" component={Works} />
-      <Route path="/contact" component={Contact} />
-      <Route component={NotFoundPage} />
-    </Switch>
+    <Router basename={process.env.PUBLIC_URL}>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/home">
+          <Redirect to="/" />
+        </Route>
+        <Route path="/about" component={About} />
+        <Route path="/blogs" component={Blogs} />
+        <Route path="/works" component={Works} />
+        <Route path="/contact" component={Contact} />
+        <Route component={NotFoundPage} />
+      </Switch>
+    </Router>
   );
 }
 
